bf86d0f0338c6f6258057cb4e09677bc7c0c5d45
RubyG <rubygillr@gmail.com>
2018-06-24 23:36:29 -0400

    pseudocode added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,54 @@ $(document).ready(function () {
 
 })
 
+//VARIABLES 
+//-------------------------------------------
 
+//Create a var to store target#
+
+//Create a counter var with 0 value to compare
+
+//Create var to store random number array 
+
+
+
+//FUNCTIONS/CONDITIONS/LOOPS
+//--------------------------------------------
+
+//Create loop to generate random target number 
+
+    //Then append random number to html id 
+
+
+//Create a loop to generate random value to counter from number options array
+    //create a var [ imageName = $("<img>"); ] to create a new html image section
+        //add a class to the image 
+            //add attribute, image source 
+                //assign attribute to imageName, with string, and value of the number options[i]
+
+                //append it all to the div or section in html 
+                //repeat based on the # of jewels
+
+    //create a function for a click on image 
+        //create a value to store the string value from imageName using attr() 
+        //crystalValue = parseInt("the variable from above");
+            //counter += value;
+            //post counter; 
+
+            //create if coundition for win/loss 
+                //counter===targetnum
+
+
+
+
+
+
+
+
+
+
+
+//CALLS
 
 
 
