3104e64d881f58a23dd9a6e05b6fe4a634b1d388
RubyG <rubygillr@gmail.com>
2018-07-03 10:18:46 -0400

    Js Complete, changed as reset function was not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,134 +2,104 @@ $(document).ready(function () {
 
     //VARIABLES 
     //-------------------------------------------
-    var value = 0;
+    var gems = {
+        redGem:
+        {
+            color: "red",
+            value: 0
+        },
+        blueGem:
+        {
+            color: "blue",
+            value: 0
+        },
+        greenGem:
+        {
+            color: "green",
+            value: 0
+        },
+        diamondGem:
+        {
+            color: "diamond",
+            value: 0
+        },
+    };
+
+    var targetNum = 0;
+    var counter = 0;
+
     var wins = 0;
     var losses = 0;
-    var targetNum = [Math.floor(Math.random() * 102) + 19];
-    console.log("This is " + targetNum);
-
 
     //FUNCTIONS/CONDITIONS/LOOPS
-    //-------------------------------------------
-    function startGame() {
-        //Then append random number to html id 
+    //--------------------------------------------
+    function start() {
+        counter = 0;
+        //Generate random # for target 
+        targetNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        console.log("This is " + targetNum);
+        //random number to html id 
         $("#targetnum").html(targetNum);
 
-        //JEWEL ONE______________________________________
-        value = [Math.floor(Math.random() * 12) + 1]
-
-        console.log(" The value is  " + value)
-        //create a var [ imageName = $("<img>"); ] to create a new html image section
-
-        var gemImage = $("<img>");
-        //add a class to the image 
-        gemImage.addClass("gemmy")
-        //add attribute, image source 
-        gemImage.attr("src", "./assets/images/redgem.jpg");
-
-        console.log(gemImage);
-
-        //assign attribute to imageName, with string, and value of the number options[i]
-        gemImage.attr("data-Gems", value);
-
-        //append it all to the div or section in html 
-        $("#jewel").append(gemImage);
+        gems.redGem.value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        gems.blueGem.value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        gems.greenGem.value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        gems.diamondGem.value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
 
-        //JEWEL TWO _____________________________________
+        $("#yournum").html(counter);
 
-        valueTwo = [Math.floor(Math.random() * 12) + 1]
-        console.log(" The value is  " + value)
-        var gemImageTwo = $("<img>");
-        //add a class to the image 
-        gemImageTwo.addClass("gemmy")
-        //add attribute, image source 
-        gemImageTwo.attr("src", "./assets/images/bluegem.jpg");
+    };
 
-        console.log(gemImageTwo);
+    //-------------------------------------
+    //NEW FUNCTION (ADD VALUES)
+    function adding(gems) {
+        counter = counter + gems.value;
+        $("#yournum").html(counter);
+        check();
+        console.log("Your Score is  " + counter);
+    }
+    //-----------------------------------
+    //another function
+    function check() {
+        if (counter > targetNum) {
+            alert("You LOST!");
 
-        //assign attribute to imageName, with string, and value of the number options[i]
-        gemImageTwo.attr("data-Gems", valueTwo);
-
-        //append it all to the div or section in html 
-        $("#jewel").append(gemImageTwo);
-
-
-
-        //JEWEL THREE ___________________________________________
-        valueThree = [Math.floor(Math.random() * 12) + 1]
-        console.log(" The value is  " + value)
-        var gemImageThree = $("<img>");
-        //add a class to the image 
-        gemImageThree.addClass("gemmy")
-        //add attribute, image source 
-        gemImageThree.attr("src", "./assets/images/diamondgem.jpg");
-
-        console.log(gemImageThree);
-
-        //assign attribute to imageName, with string, and value of the number options[i]
-        gemImageThree.attr("data-Gems", valueThree);
-
-        //append it all to the div or section in html 
-        $("#jewel").append(gemImageThree);
-
-
-        //JEWEL FOUR_________________________________________
-        valueFour = [Math.floor(Math.random() * 12) + 1]
-        console.log(" The value is  " + value)
-        var gemImageFour = $("<img>");
-        //add a class to the image 
-        gemImageFour.addClass("gemmy")
-        //add attribute, image source 
-        gemImageFour.attr("src", "./assets/images/greengem.jpg");
-
-        console.log(gemImageFour);
-
-        //assign attribute to imageName, with string, and value of the number options[i]
-        gemImageFour.attr("data-Gems", valueFour);
-
-        //append it all to the div or section in html 
-        $("#jewel").append(gemImageFour);
-
-
-
-
-        //create a function for a click on image 
-        var counter = 0;
-
-
-        $(".gemmy").on("click", function () {
-            var gemValue = ($(this).attr("data-Gems"));
-            gemValue = parseInt(gemValue);
-
-            counter += gemValue;
-
-            alert("you clicked  " + counter + "  times!")
-
-
-
-
-
-            startGame()
-
-            //create if coundition for win/loss 
-
-            if (counter === targetNum) {
-                wins++;
-                $("#wins").html(wins);
-                startGame();
-
-
-            }
-            else if (counter >= targetNum)
-                losses++;
+            console.log("YOU LOST");
+            losses++;
             $("#loss").html(losses);
-            startGame();
+            start();
 
-        });
+        }
+        else if (counter == targetNum) {
+            alert("You WIN!");
+            console.log("YOU WIN!");
+            wins++;
+            $("#wins").html(wins);
+            start();
+        }
+    }
 
+    //------------------------------------\
+    start()
+
+    $("#red").on("click", function () {
+        adding(gems.redGem);
     });
-//CALLS
+
+    $("#blue").on("click", function () {
+        adding(gems.blueGem);
+    });
+    $("#diamond").on("click", function () {
+        adding(gems.greenGem);
+    });
+    $("#green").on("click", function () {
+        adding(gems.diamondGem);
+    });
+
+
+});
+
 
 
 
